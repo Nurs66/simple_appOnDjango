f4ce2e323aea1f800c8e04fdcb201b9164f0afde
PythonNurs <eldosnursultan@gmail.com>
2019-04-20 12:43:06 +0600

    django simple blog

---
+++ django_project/blog/forms.py
@@ -0,0 +1,21 @@
+from .models import *
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content',]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+# class PostImageForm(forms.ModelForm):
+#     class Meta:
+#         model = PostImage
+#         fields = ['image', ]
+#         widgets = {
+#             'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+#         }
