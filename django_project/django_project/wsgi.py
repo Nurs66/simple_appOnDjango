64eebe5f9217521f7cbc84a68bec079498ba140f
PythonNurs <eldosnursultan@gmail.com>
2019-07-06 23:10:04 +0600

    add django_app Procfile

--- django_project/django_project/wsgi.py
+++ django_project/django_project/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from .settings import*
+from .settings import *
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
